d4865fe70b478f2e6736484d2bc0d71e6b93880f
sseigel8 <sseigel8@gmail.com>
2025-04-29 09:58:54 -0500

    Update app_config.php

--- app_config.php
+++ app_config.php
@@ -2,7 +2,7 @@
 //application folder name on your web root or htdocs folder
 define('APP_ROOT',$_SERVER['DOCUMENT_ROOT']);
 //DEBUG echo (APP_ROOT)."      <br>";
-define('APP_FOLDER_NAME', '/');
+define('APP_FOLDER_NAME', '/ACME_Medical');
 define('WEB_ROOT','http://'.$_SERVER['SERVER_NAME']);
 //DEBUG echo(WEB_ROOT);
 define('DSN1', 'mysql:host=localhost;dbname=');
